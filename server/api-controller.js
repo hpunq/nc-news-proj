7246362f5af7b3a00f2d0952ca247241884fbe10
hpunq <128979576+hpunq@users.noreply.github.com>
2024-11-29 15:30:46 +0000

    refactor earlier tasks and complete task 8

--- server/api-controller.js
+++ server/api-controller.js
@@ -6,10 +6,11 @@ const {
   selectArticlesX,
   selectComments,
   addComment,
+  updateArticleVote
 } = require("./app-model");
 
 function getApi(req, res) {
- res.status(200).send({ endpoints });
+  res.status(200).send({ endpoints });
 }
 
 function getTopics(req, res) {
@@ -17,29 +18,40 @@ function getTopics(req, res) {
 }
 
 function getArticle(req, res) {
- 
   const articleID = req.params.article_id;
   selectArticle(articleID).then((article) => res.status(200).send({ article }));
 }
 
 function getArticlesX(req, res) {
- 
   selectArticlesX().then((articles) => res.status(200).send({ articles }));
 }
 
 function getComments(req, res) {
   const articleID = req.params.article_id;
- 
-  selectComments(articleID).then((comments) => res.status(200).send({ comments }));
+
+  selectComments(articleID).then((comments) =>
+    res.status(200).send({ comments })
+  );
 }
 
 function postComment(req, res) {
   const comment = req.body;
   const articleID = req.params.article_id;
-  addComment(comment, articleID)
-  .then((newComment) => {
-    res.status(201).send({newComment});
+  addComment(comment, articleID).then((newComment) => {
+    res.status(201).send({ newComment });
   });
+}
+
+function patchArticleVote(req, res){
+    const voteValue = req.body.inc_votes
+    const articleID = req.params.article_id
+
+    if (!voteValue) res.status(400).send({errorResponse: "Bad Request"})
+    if (typeof(voteValue) !== "number") res.status(400).send({errorResponse: "Bad Request"})
+    
+    else updateArticleVote(voteValue, articleID).then((updatedArticle) => {
+        res.status(200).send({updatedArticle})
+    })
 }
 
 module.exports = {
@@ -49,4 +61,5 @@ module.exports = {
   getArticlesX,
   getComments,
   postComment,
+  patchArticleVote
 };
