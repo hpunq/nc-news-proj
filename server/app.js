0b61c201ac08050d1a907995af7eb368aefd3bdb
hpunq <128979576+hpunq@users.noreply.github.com>
2024-12-16 10:22:07 +0000

    allow cross origin resource sharing

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
-const { getApi, getTopics, getArticle, getArticlesX, getComments, postComment, patchArticleVote, deleteArticleComment } = require("./api-controller");
-app.use(express.json())
+const {
+  getApi,
+  getTopics,
+  getArticle,
+  getArticlesX,
+  getComments,
+  postComment,
+  patchArticleVote,
+  deleteArticleComment,
+} = require("./api-controller");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors())
 
 app.get("/api", getApi);
 
@@ -11,34 +23,32 @@ app.get("/api/articles/:article_id", getArticle);
 
 app.get("/api/articles", getArticlesX);
 
-app.get("/api/articles/:article_id/comments", getComments)
+app.get("/api/articles/:article_id/comments", getComments);
 
-app.post("/api/articles/:article_id/comments", postComment)
+app.post("/api/articles/:article_id/comments", postComment);
 
-app.patch("/api/articles/:article_id", patchArticleVote)
+app.patch("/api/articles/:article_id", patchArticleVote);
 
-app.delete("/api/comments/:comment_id", deleteArticleComment)
-
-app.use((err, req, res, next) => {
-    if (err.code === "22P02") {
-        res.status(400).send({errorResponse: "Bad Request"})
-    }
-    else next(err)
-})
+app.delete("/api/comments/:comment_id", deleteArticleComment);
 
 app.use((err, req, res, next) => {
-    if (err.status){
-        res.status(err.status).send({errorResponse: err.errorResponse})
-    }
-    else next(err)
-})
+  if (err.code === "22P02") {
+    res.status(400).send({ errorResponse: "Bad Request" });
+  } else next(err);
+});
 
 app.use((err, req, res, next) => {
-    res.status(500).send({errorResponse: "Internal server error"})
-})
+  if (err.status) {
+    res.status(err.status).send({ errorResponse: err.errorResponse });
+  } else next(err);
+});
 
-app.use((req, res, next) =>{
-    res.status(404).send({errorResponse: "Content not found"});
+app.use((err, req, res, next) => {
+  res.status(500).send({ errorResponse: "Internal server error" });
+});
+
+app.use((req, res, next) => {
+  res.status(404).send({ errorResponse: "Content not found" });
 });
 
 module.exports = app;
